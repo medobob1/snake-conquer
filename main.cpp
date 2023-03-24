ef904acbe9311309cfe0712d11ba500243431662
Mohamed Adel <moadelwo77@gmail.com>
2023-03-24 23:30:24 +0200

    Added all project files

--- main.cpp
+++ main.cpp
@@ -1,5 +1,8 @@
 #include <GL/glut.h>
+
 void display();
+void init();
+
 int main(int argc, char* argv[]) {
     glutInit(&argc, argv);
 
@@ -7,8 +10,25 @@ int main(int argc, char* argv[]) {
     glutInitWindowSize(600, 400);
 
     glutCreateWindow("Snake Conquer");
+    init();
     glutDisplayFunc(display);
 
     glutMainLoop();
 }
-void display(){}
+void display()
+{
+    glClear(GL_COLOR_BUFFER_BIT);
+    glColor3f(1.0, 1.0, 1.0);
+    glBegin(GL_POLYGON);
+    glVertex3f(0.25, 0.25, 0.0);
+    glVertex3f(0.75, 0.25, 0.0);
+    glVertex3f(0.75, 0.75, 0.0);
+    glVertex3f(0.25, 0.75, 0.0);
+    glEnd();
+    glFlush();
+}
+void init()
+{
+    glLoadIdentity();
+    glOrtho(0.0, 1.0, 0.0, 1.0, -1.0, 1.0);
+}
