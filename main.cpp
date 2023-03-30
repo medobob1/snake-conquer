c834ac92ec43ff45c0db8d51ed8610b9f00467c5
Mohamed Adel <moadelwo77@gmail.com>
2023-03-30 17:04:39 +0200

    Added the ability to draw a line using DDA algorithm

--- main.cpp
+++ main.cpp
@@ -1,34 +1,72 @@
 #include <GL/glut.h>
+#include <iostream>
+#include <chrono>
+#include <thread>
 
-void display();
-void init();
-
-int main(int argc, char* argv[]) {
-    glutInit(&argc, argv);
-
-    glutInitWindowPosition(200, 200);
-    glutInitWindowSize(600, 400);
-
-    glutCreateWindow("Snake Conquer");
-    init();
-    glutDisplayFunc(display);
-
-    glutMainLoop();
+void delay(int milliseconds) {
+    std::this_thread::sleep_for(std::chrono::milliseconds(milliseconds));
 }
-void display()
-{
+
+void display() {
+    glClearColor(0, 0, 0, 0);
     glClear(GL_COLOR_BUFFER_BIT);
-    glColor3f(1.0, 1.0, 1.0);
-    glBegin(GL_POLYGON);
-    glVertex3f(0.25, 0.25, 0.0);
-    glVertex3f(0.75, 0.25, 0.0);
-    glVertex3f(0.75, 0.75, 0.0);
-    glVertex3f(0.25, 0.75, 0.0);
-    glEnd();
+    glColor3f(1, 0, 0);
+    std::cout<<"Enter x1, y1, x2, y2:"<< "\n";
+    float x1;
+    std::cin>>x1;
+    float y1;
+    std::cin>>y1;
+    float x2;
+    std::cin>>x2;
+    float y2;
+    std::cin>>y2;
+    float dx = x2 - x1;
+    float dy = y2 - y1;
+    float steps = dx > dy ? dx : dy;
+    float xInc = dx / steps;
+    float yInc = dy / steps;
+    glPointSize(5);
+    float x = x1;
+    float y = y1;
+    for (int i = 0; i <= steps; i++) {
+        glBegin(GL_POINTS);
+        glVertex2f(x, y);
+        glEnd();
+        glFlush();
+        std::this_thread::sleep_for(std::chrono::milliseconds(20)); // Increase delay time
+        x += xInc;
+        y += yInc;
+        if (i == steps) {
+            std::cout << "Done" << "\n";
+            std::cout << "If you want to draw a new line enter 1 else enter 0" << "\n";
+            int choice;
+            std::cin >> choice;
+            if (choice == 1) {
+                display();
+            }
+            else if (choice != 0 && choice != 1) {
+                std::cout << "Invalid choice" << "\n";
+                display();
+            }
+             else if (choice == 0) {
+                exit(0);
+            }   
+        }
+    }
     glFlush();
 }
-void init()
-{
+
+
+
+int main(int argc, char** argv) {
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+    glutInitWindowPosition(100, 100);
+    glutInitWindowSize(500, 500);
+    glutCreateWindow("Hello World");
+    glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
-    glOrtho(0.0, 1.0, 0.0, 1.0, -1.0, 1.0);
+    gluOrtho2D(0, 500, 0, 500);
+    glutDisplayFunc(display);
+    glutMainLoop();
 }
